3056a4027e49ffac137f34446ef2fe6c4e4eb5cd
mastervic <56386068+mastervic@users.noreply.github.com>
2020-02-11 18:10:29 +0200

    Created a test unit for baskets

---
+++ MyShop/MyShop.WebUI.Tests/Controllers/BasketControllerTest.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using MyShop.Core.ViewModels;
+using MyShop.Services;
+using MyShop.WebUI.Controllers;
+using MyShop.WebUI.Tests.Mocks;
+
+namespace MyShop.WebUI.Tests.Controllers
+{
+    [TestClass]
+    public class BasketControllerTest
+    {
+        [TestMethod]
+        public void CanAddBasketItem()
+        {
+            //setup 
+            IRepository<Basket> baskets = new MockContext<Basket>();
+            IRepository<Product> products = new MockContext<Product>();
+
+            var httpContext = new MockHttpContext();
+            
+            IBasketService basketService = new BasketService(products, baskets);
+
+            var controller = new BasketController(basketService);
+
+            controller.ControllerContext = new System.Web.Mvc.ControllerContext(httpContext, new System.Web.Routing.RouteData(), controller);
+
+            //Act - Run your tests
+            //basketService.addToBasket(httpContext, "1");
+
+            controller.AddToBasket("1");
+
+            Basket basket = baskets.Collection().FirstOrDefault();
+
+
+            //Assert
+            Assert.IsNotNull(basket);
+            Assert.AreEqual(1, basket.BasketItems.Count);
+            Assert.AreEqual("1", basket.BasketItems.ToList().FirstOrDefault().ProductId);
+        }
+
+        [TestMethod]
+        public void CanGetSummaryViewModel()
+        {
+            //setup 
+            IRepository<Basket> baskets = new MockContext<Basket>();
+            IRepository<Product> products = new MockContext<Product>();
+
+            products.Insert(new Product() { Id = "1", Price = 10.00m });
+            products.Insert(new Product() { Id = "2", Price = 50.00m });
+            products.Insert(new Product() { Id = "3", Price = 90.00m });
+            products.Insert(new Product() { Id = "4", Price = 70.00m });
+            products.Insert(new Product() { Id = "5", Price = 20.00m });
+
+            Basket basket = new Basket();
+
+            basket.BasketItems.Add(new BasketItem() { ProductId = "1", Quantity = 2 });
+            basket.BasketItems.Add(new BasketItem() { ProductId = "2", Quantity = 3 });
+            basket.BasketItems.Add(new BasketItem() { ProductId = "3", Quantity = 1 });
+            basket.BasketItems.Add(new BasketItem() { ProductId = "4", Quantity = 4 });
+            basket.BasketItems.Add(new BasketItem() { ProductId = "5", Quantity = 6 });
+
+            baskets.Insert(basket);
+
+            IBasketService basketService = new BasketService(products, baskets);
+
+            var controller = new BasketController(basketService);
+
+            var httpContext = new MockHttpContext();
+
+            httpContext.Request.Cookies.Add(new System.Web.HttpCookie("eCommerceBasket") { Value = basket.Id });
+            controller.ControllerContext = new System.Web.Mvc.ControllerContext(httpContext, new System.Web.Routing.RouteData(), controller);
+
+            var result = controller.BasketSummary() as PartialViewResult;
+            var basketSummary = (BasketSummaryViewModel)result.ViewData.Model;
+
+            Assert.AreEqual(16, basketSummary.BasketCount);
+            Assert.AreEqual(660, basketSummary.BasketTotal);
+        }
+    }
+}
---
+++ MyShop/MyShop.WebUI.Tests/Mocks/MockHttpContext.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace MyShop.WebUI.Tests.Mocks
+{
+    public class MockHttpContext : HttpContextBase
+    {
+        private MockResponse response;
+        private MockRequest request;
+        private HttpCookieCollection httpCookie;
+
+        public MockHttpContext()
+        {
+            httpCookie = new HttpCookieCollection();
+            this.request = new MockRequest(httpCookie);
+            this.response = new MockResponse(httpCookie);
+        }
+
+        public override HttpRequestBase Request
+        {
+            get
+            {
+                return request;
+            }
+        }
+
+        public override HttpResponseBase Response
+        {
+            get
+            {
+                return response;
+            }
+        }
+    }
+
+    public class MockResponse : HttpResponseBase
+    {
+        private readonly HttpCookieCollection httpCookie;
+
+        public MockResponse(HttpCookieCollection httpCookie)
+        {
+            this.httpCookie = httpCookie;
+        }
+
+        public override HttpCookieCollection Cookies
+        {
+            get
+            {
+                return httpCookie;
+            }
+        }
+    }
+    public class MockRequest : HttpRequestBase
+    {
+        private readonly HttpCookieCollection httpCookie;
+
+        public MockRequest(HttpCookieCollection httpCookie)
+        {
+            this.httpCookie = httpCookie;
+        }
+
+        public override HttpCookieCollection Cookies
+        {
+            get
+            {
+                return httpCookie;
+            }
+        }
+    }
+}
