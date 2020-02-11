4e108334aa82d2d9284b32bf909e1c17e9f5ae33
mastervic <56386068+mastervic@users.noreply.github.com>
2020-02-11 14:44:55 +0200

    finished basket services

---
+++ MyShop/MyShop.Core/Contracts/IBasketService.cs
@@ -0,0 +1,18 @@
+﻿using MyShop.Core.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace MyShop.Core.Contracts
+{
+    public interface IBasketService
+    {
+        void addToBasket(HttpContextBase httpContext, string productId);
+        void removeFromBasket(HttpContextBase httpContext, string itemId);
+        List<BasketItemViewModel> GetBasketItems(HttpContextBase httpContext);
+        BasketSummaryViewModel GetBasketSummary(HttpContextBase httpContext);
+    }
+}
