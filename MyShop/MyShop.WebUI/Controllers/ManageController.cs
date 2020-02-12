ec060551b908938c56114d22fd9e22148436c9dc
mastervic <56386068+mastervic@users.noreply.github.com>
2020-02-12 11:42:00 +0200

    added customer info registration and database table

--- MyShop/MyShop.WebUI/Controllers/ManageController.cs
+++ MyShop/MyShop.WebUI/Controllers/ManageController.cs
@@ -18,13 +18,7 @@ namespace MyShop.WebUI.Controllers
 
         public ManageController()
         {
-        }
-
-        public ManageController(ApplicationUserManager userManager, ApplicationSignInManager signInManager)
-        {
-            UserManager = userManager;
-            SignInManager = signInManager;
-        }
+        }       
 
         public ApplicationSignInManager SignInManager
         {
